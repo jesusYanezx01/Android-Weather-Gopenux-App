aa595d778200d94766bb6c00e123d567fde58493
Jesus Arturo Yañez Patiño <jesusmitesoro2004@gmail.com>
2024-04-24 09:01:21 -0500

    implementation of activities for the views that will be used

--- app/src/main/java/com/example/androidopenweathergopenux/presentation/ui/MainActivity.java
+++ app/src/main/java/com/example/androidopenweathergopenux/presentation/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.androidopenweathergopenux;
+package com.example.androidopenweathergopenux.presentation.ui;
 
 import android.os.Bundle;
 
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.androidopenweathergopenux.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +17,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
---
+++ app/src/main/java/com/example/androidopenweathergopenux/presentation/ui/WeatherActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidopenweathergopenux.presentation.ui;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.androidopenweathergopenux.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
